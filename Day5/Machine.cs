5b2da19a4f64ed762b0f9b856f15944c6c709703
Didzis Klavins <didzis.klavins@visma.com>
2019-12-05 13:48:18 +0100

    Day 5 completed

--- Day5/Machine.cs
+++ Day5/Machine.cs
@@ -4,70 +4,133 @@ using System.Text;
 
 namespace Day5
 {
-	class Machine
+	public class Machine
 	{
 		public static int ArgMode(int code, int mask)
 		{
-			return (code % (mask * 10) - (code % mask))/mask;
+			return (code % (mask * 10) - (code % mask)) / mask;
 		}
-		public static int CalcWithInput(int verb, int noun, int[] memory)
+		public static int CalcWithInput(int verb, int noun, int[] memory, Action<string, int, int> outputFunc)
 		{
 
 			memory[1] = verb;
 			memory[2] = noun;
-			return Calc(memory);
+			return Calc(memory, 0, outputFunc);
 		}
-			public static int Calc(int[] memory)
-			{
-				int pc = 0;
-				while (pc < memory.Length)
+
+		public static int Calc(int[] memory, int singleInput, Action<string, int, int> outputFunc)
+		{
+			int pc = 0;
+			while (pc < memory.Length)
 			{
 				var opcode = memory[pc] % 100;
-				var arg1mode = ArgMode(memory[pc], 100);
-				var arg2mode = ArgMode(memory[pc], 1000);
-				var arg3mode = ArgMode(memory[pc], 10000);
+				var arg1Mode = ArgMode(memory[pc], 100);
+				var arg2Mode = ArgMode(memory[pc], 1000);
+				var arg3Mode = ArgMode(memory[pc], 10000);
+				int getArg1()
+				{
+					switch (opcode)
+					{
+						case 1:
+						case 2:
+						case 3:
+						case 4:
+						case 5:
+						case 6:
+						case 7:
+						case 8:
+							var addr = memory[pc + 1];
+							int arg = addr;
+							if (arg1Mode == 0)
+								arg = memory[addr];
+							return arg;
+					}
 
+					return 0;
+				}
+				int getArg2()
+				{
+					switch (opcode)
+					{
+						case 1:
+						case 2:
+						case 5:
+						case 6:
+						case 7:
+						case 8:
+							var addr = memory[pc + 2];
+							int arg = addr;
+							if (arg2Mode == 0)
+								arg = memory[addr];
+							return arg;
+					}
+
+					return 0;
+
+				}
+				int getArg3()
+				{
+					switch (opcode)
+					{
+						case 7:
+						case 8:
+							var addr = memory[pc + 3];
+							int arg = addr;
+							return arg;
+					}
+
+					return 0;
+				}
+				var arg1 = getArg1();
+				var arg2 = getArg2();
+				var arg3 = getArg3();
 				switch (opcode)
 				{
 					case 1:
-						var addr1 = memory[pc + 1];
-						int arg1=addr1;
-						if (arg1mode == 0)
-							arg1 = memory[addr1];
-						var addr2 = memory[pc + 2];
-						int arg2 = addr2;
-						if (arg2mode == 0)
-							arg2 = memory[addr2];
 						var addrt = memory[pc + 3];
 						memory[addrt] = arg1 + arg2;
 						pc = pc + 4;
 						break;
 					case 2:
-						var addr21 = memory[pc + 1];
-						int arg21 = addr21;
-						if (arg1mode == 0)
-							arg21 = memory[addr21];
-						var addr22 = memory[pc + 2];
-						int arg22 = addr22;
-						if (arg2mode == 0)
-							arg22 = memory[addr22];
-						var addrt2 = memory[pc + 3];
-						memory[addrt2] = arg21 * arg22;
+						addrt = memory[pc + 3];
+						memory[addrt] = arg1 * arg2;
 						pc = pc + 4;
 						break;
 					case 3:
-						int singleInput = 1;
 						var addr31 = memory[pc + 1];
 						memory[addr31] = singleInput;
 						pc = pc + 2;
 						break;
 					case 4:
-						var addr41 = memory[pc + 1];
-						int arg41 = addr41;
-						if (arg1mode == 0)
-							arg41 = memory[addr41];
-						Console.WriteLine($"Output {pc} {arg41}");
+						arg1 = getArg1();
+						outputFunc("Output {0} {1}", pc, arg1);
 						pc = pc + 2;
+						break;
+					case 5: //JNZ
+						if (arg1 != 0)
+							pc = arg2;
+						else
+							pc = pc + 3;
+						break;
+					case 6: //JZ
+						if (arg1 == 0)
+							pc = arg2;
+						else
+							pc = pc + 3;
+						break;
+					case 7: //LT
+						if (arg1 < arg2)
+							memory[arg3] = 1;
+						else
+							memory[arg3] = 0;
+						pc = pc + 4;
+						break;
+					case 8: //EQ
+						if (arg1 == arg2)
+							memory[arg3] = 1;
+						else
+							memory[arg3] = 0;
+						pc = pc + 4;
 						break;
 					case 99:
 						return memory[0];
