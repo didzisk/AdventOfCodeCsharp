499e817c4029193c6fad7b05d740471932a04c33
Didzis Klavins <didzis.klavins@visma.com>
2019-12-18 09:58:26 +0100

    added Day18 grid read

---
+++ Day18/Day18Calc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day18
+{
+    public class Day18Calc
+    {
+        public static void CalcMain()
+        {
+
+            var lines = Day18Input.Day18Maze.Split("\r\n");
+            foreach (var item in lines)
+            {
+
+            }
+        }
+    }
+}
