6f82b20f7d168063f7d550fc2be6b0231ba45862
didzisk <didzis.klavins@gmail.com>
2019-12-19 06:36:42 +0100

    added Day19 pt1

---
+++ Day19/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Day19Calc.Calc();
+        }
+    }
+}
